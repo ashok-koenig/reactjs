297601b6dc4ff90e5a36a544f122b7b75e9fc281
Ashok S <ashok.s@koenig-solutions.com>
2025-02-04 14:40:16 +0530

    global variable

--- 01-JavaScript-Basics/01-variables.js
+++ 01-JavaScript-Basics/01-variables.js
@@ -1,1 +1,7 @@
 console.log("Working with JavaScript Variables");
+
+// Creating a global variable and initializing with a string value.
+var globalVariable = "This is global variable";
+
+// Print the variable value into console
+console.log(globalVariable);
