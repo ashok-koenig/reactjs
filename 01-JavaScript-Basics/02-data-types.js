d23bf64235aff2df0f1af81151c6d2a3bf32a6d2
Ashok S <ashok.s@koenig-solutions.com>
2025-02-04 15:12:30 +0530

    typeof operator

--- 01-JavaScript-Basics/02-data-types.js
+++ 01-JavaScript-Basics/02-data-types.js
@@ -4,3 +4,7 @@ var age = 25;
 var isStudent = false
 
 console.log(name, age, isStudent)
+
+console.log("name data type is " + (typeof name))
+console.log("age data type is " + (typeof age))
+console.log("isStudent data type is " + (typeof isStudent))
