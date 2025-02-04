741dde02ffcde978b39a3230c216bb3beb7555bb
Ashok S <ashok.s@koenig-solutions.com>
2025-02-04 14:45:28 +0530

    block variable

--- 01-JavaScript-Basics/01-variables.js
+++ 01-JavaScript-Basics/01-variables.js
@@ -1,7 +1,13 @@
 console.log("Working with JavaScript Variables");
 
-// Creating a global variable and initializing with a string value.
+// Creating a global variable with var keyword and initializing with a string value.
 var globalVariable = "This is global variable";
 
 // Print the variable value into console
 console.log(globalVariable);
+
+// Simple block
+{
+    let blockVariable = "This is block variable"
+    console.log(blockVariable)
+}
