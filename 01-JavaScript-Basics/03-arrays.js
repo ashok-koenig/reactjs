0ed8bdb9926248178b1a1866a87bb2efa399baf1
Ashok S <ashok.s@koenig-solutions.com>
2025-02-04 15:22:13 +0530

    simple array

---
+++ 01-JavaScript-Basics/03-arrays.js
@@ -0,0 +1,14 @@
+// Creating an array
+var marks = [100, 99, 78, 70]
+
+// Display all the array values
+console.log(marks)
+
+// Access the first array value (index 0)
+console.log(marks[0])
+
+// Modify second array value (index 1)
+marks[1]= 100
+
+// Display all the array values
+console.log(marks)
