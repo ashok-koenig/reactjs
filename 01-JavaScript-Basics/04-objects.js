a39229c7f6787ac0818f2aa27256266290459ac9
Ashok S <ashok.s@koenig-solutions.com>
2025-02-05 12:24:26 +0530

    objects

---
+++ 01-JavaScript-Basics/04-objects.js
@@ -0,0 +1,18 @@
+// Creating an object
+const person = { name: "John smith", age: 25, isStudent: false }
+
+// Access a value with dot notation
+console.log(person.name)
+
+// Access a value with bracket notation
+console.log(person['age'])
+
+// Display all object properies 
+console.log(person)
+
+// Modify values
+person.age = 26
+person["isStudent"]=true
+
+// Display all object properies 
+console.log(person)
