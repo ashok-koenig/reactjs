797bd38988bdfd8d9c48235087b7c36e7907f616
Ashok S <ashok.s@koenig-solutions.com>
2025-02-05 13:26:42 +0530

    arrow function

--- 01-JavaScript-Basics/07-arrow-function.js
+++ 01-JavaScript-Basics/07-arrow-function.js
@@ -12,9 +12,11 @@ const add = (num1, num2)=>{
 
 add(100, 200)
 
-const multiply = (num1, num2) => {
-    return num1 * num2
-}
+// const multiply = (num1, num2) => {
+//     return num1 * num2
+// }
+// We can skip curly braces and return keyword if only the return statement is the function body. 
+const multiply = (num1, num2) => num1 * num2
 
 let result = multiply(10, 5)
 console.log("Product is "+ result)
