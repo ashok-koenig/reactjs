a6e34f52fa7f7be06b920e081383def8bdae92ef
Ashok S <ashok.s@koenig-solutions.com>
2025-02-05 12:50:28 +0530

    function with parameter

--- 01-JavaScript-Basics/05-functions.js
+++ 01-JavaScript-Basics/05-functions.js
@@ -5,3 +5,12 @@ function greet() {
 
 // Calling a function
 greet()
+
+// Function with parameters
+function add(num1, num2){
+    let result = num1 + num2;
+    console.log("Result is "+ result)
+}
+
+// Calling function with parameters
+add(10, 20)
