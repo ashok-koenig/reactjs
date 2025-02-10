e55c99a138d5b95fd8d81711ef466f5b2d91825d
Ashok S <ashok.s@koenig-solutions.com>
2025-02-10 13:29:52 +0530

    list demo app

---
+++ Module-02/list-demo-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NameList from './components/NameList'
+
+function App() {  
+
+  return (
+    <>
+      <h1>Working with List and Conditional Rendering</h1>
+      <NameList />
+    </>
+  )
+}
+
+export default App
