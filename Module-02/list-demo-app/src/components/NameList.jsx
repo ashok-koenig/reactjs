806fb77d1302d318fbabf1979cd1526ca1d9f1ab
Ashok S <ashok.s@koenig-solutions.com>
2025-02-10 14:04:11 +0530

    name list rendering

--- Module-02/list-demo-app/src/components/NameList.jsx
+++ Module-02/list-demo-app/src/components/NameList.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
 function NameList() {
+    const names = ["John", "Smith", "Peter", "Bob"]
   return (
-    <div>NameList</div>
+    <div>
+        <h2>Rendering static list</h2>
+        {/* <ol>
+            <li>{names[0]}</li>
+            <li>{names[1]}</li>
+            <li>{names[2]}</li>
+            <li>{names[3]}</li>
+        </ol> */}
+        <ol>
+            {names.map((name)=> <li>{name}</li>)}
+        </ol>
+    </div>
   )
 }
 
