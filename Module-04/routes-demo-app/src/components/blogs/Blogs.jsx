baf0489bbd2d9b85a22279718c602b0535afdcb8
Ashok S <ashok.s@koenig-solutions.com>
2025-02-14 14:00:53 +0530

    nested routes with links

--- Module-04/routes-demo-app/src/components/blogs/Blogs.jsx
+++ Module-04/routes-demo-app/src/components/blogs/Blogs.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
 
 function Blogs() {
   return (
-    <div>Blogs</div>
+    <div>
+        <h2>Blogs Page</h2>
+        <hr/>
+        <Link to="/blogs">List of Blogs</Link> | 
+        <Link to="/blogs/add">Add Blog</Link>
+        <hr/>
+        <Outlet />
+    </div>
   )
 }
 
