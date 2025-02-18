b494fe9c9ecc9d4ea49cf704e6f5fde364e8a597
Ashok S <ashok.s@koenig-solutions.com>
2025-02-18 15:22:00 +0530

    update product

--- Module-04/products-app/src/pages/EditProduct.jsx
+++ Module-04/products-app/src/pages/EditProduct.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useRef, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 function EditProduct() {
+    const {id} = useParams()
+    const titleRef = useRef()
+    const priceRef = useRef()
+    const [error, setError] = useState(null)
+    const [message, setMessage] = useState()
+
+    // Used to fill the input field from the products api
+    useEffect(()=>{
+        async function fetchData() {
+            const response = await axios.get("http://localhost:3000/products/"+id)
+            if(response.data){
+                titleRef.current.value = response.data.title;
+                priceRef.current.value = response.data.price;
+            }            
+        }
+        fetchData()
+    }, [id])
+
+
+    async function updateProduct(editedProduct) {
+        try{
+            const response = await axios.put("http://localhost:3000/products/"+id, editedProduct)
+            console.log(response.data)
+            if(response.data.id){
+                setMessage("Product updated with id "+ response.data.id)
+            }
+        }catch(err){
+            setError(err.message)
+        }
+    }
+
+    const handleSubmit = (event)=>{
+        event.preventDefault()
+        const title = titleRef.current.value
+        const price = priceRef.current.value
+        if(title && price){
+            // console.log(title, price)
+            updateProduct({title, price})
+        }
+    }
   return (
-    <div>EditProduct</div>
+    <div>
+        <h2>Edit product</h2>
+        <form onSubmit={handleSubmit}>
+            <div>
+                Title: <input type='text' ref={titleRef} />
+            </div>
+            <div>
+                Price: <input type='number' ref={priceRef} />
+            </div>
+            <div>
+                <button type='submit'>Update Product</button>
+            </div>
+            <div className='error'>{error}</div>
+            <div className='success'>{message}</div>
+        </form>
+    </div>
   )
 }
 
--- Module-04/products-app/src/pages/ListProducts.jsx
+++ Module-04/products-app/src/pages/ListProducts.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function ListProducts() {
     const [products, setProducts] = useState([])
@@ -22,6 +23,24 @@ function ListProducts() {
         loadAllProducts()
     }, [])
 
+    async function handleDelete(id) {
+        const result = confirm("Are yor sure, you want to delete this product?")
+        if(result){
+            setLoading(true)
+            setError(null)
+            try{
+                await axios.delete("http://localhost:3000/products/"+id)
+                loadAllProducts()
+            }catch(err){
+                setError(err.message)
+            }finally{
+                setLoading(false)
+            }
+        }
+        
+    }
+
+
     if(loading) return <h3>Products are loading...</h3>
     if(error) return <h3>Error: {error}</h3>
 
@@ -33,6 +52,7 @@ function ListProducts() {
                     <th>ID</th>
                     <th>Title</th>
                     <th>Price</th>
+                    <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
@@ -41,6 +61,10 @@ function ListProducts() {
                                                 <td>{product.id}</td>
                                                 <td>{product.title}</td>
                                                 <td>{product.price}</td>
+                                                <td>
+                                                    <Link to={'/edit/'+product.id}>Edit</Link>
+                                                    <button onClick={()=>handleDelete(product.id)}>Delete</button>
+                                                </td>
                                             </tr>)
                 }
             </tbody>
