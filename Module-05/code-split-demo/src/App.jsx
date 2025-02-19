b61005cd2acea8cda34e7dd4a73a375044dfdbb9
Ashok S <ashok.s@koenig-solutions.com>
2025-02-19 14:35:48 +0530

    lazy loading and dynamic import

--- Module-05/code-split-demo/src/App.jsx
+++ Module-05/code-split-demo/src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import React, { Suspense, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import RootLayout from './layouts/RootLayout'
 import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
+// import About from './pages/About'
+// import Contact from './pages/Contact'
+
+// Lazy load pages
+const About = React.lazy(()=> import('./pages/About'))
+const Contact = React.lazy(()=> import('./pages/Contact'))
+
 
 const router = createBrowserRouter([
   {
@@ -14,8 +19,8 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children:[
       {index: true, element: <Home />},
-      {path: '/about', element: <About />},
-      {path: '/contact', element: <Contact />}
+      {path: '/about', element:<Suspense fallback={<p>Loading...</p>}> <About /> </Suspense> },
+      {path: '/contact', element: <Suspense fallback={<p>Loading...</p>}> <Contact /> </Suspense>}
     ]
   }
 ])
